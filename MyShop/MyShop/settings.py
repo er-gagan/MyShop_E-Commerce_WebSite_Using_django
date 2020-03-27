8daf567560ef5bb968e49069d94c494856ed77ea
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-03-27 14:19:49 +0530

    Adding Media
    Adding Media Directory and Models and Superuser Created

--- MyShop/MyShop/settings.py
+++ MyShop/MyShop/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'images')
+
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
--- MyShop/MyShopApp/models.py
+++ MyShop/MyShopApp/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    subcategory = models.CharField(max_length=50)
+    price = models.IntegerField()
     description = models.CharField(max_length=300)
     publish_date = models.DateField()
+    image = models.ImageField(upload_to="upload_images")
     
     def __str__(self):
         return self.product_name
